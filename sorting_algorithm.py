61c5cc8dfc5dbe869115351053698261099d2392
ganeshjagzap <90571701+ganeshjagzap@users.noreply.github.com>
2023-07-31 16:22:03 +0530

    Update sorting_algorithm.py

--- sorting_algorithm.py
+++ sorting_algorithm.py
@@ -119,34 +119,37 @@ print(list1)
 
 
 # merge sort
+def divide(li):
+    if(len(li)<=1):
+        return li
+    mid=len(li)//2
+    left_subarry=li[:mid]
+    right_subarray=li[mid:]
+    left_subarry=divide(left_subarry)
+    right_subarray=divide(right_subarray)
 
-def mergesort(myList):
-    if 1<len(myList):
-        mid=len(myList)//2
-        left=myList[:mid]
-        right=myList[mid:]
-        mergesort(left)
-        mergesort(right)
-        i=0
-        j=0
-        k=0
-        while i<len(left) and j<len(right):
-            if left[i]<=right[j]:
-                myList[k]=left[i]
-                i+=1
-            else:
-                myList[k]=right[j]
-                j+=1
-            k+=1
-        while i<len(left):
-            myList[k]=left[i]
+    return concur(left_subarry,right_subarray)
+
+
+def concur(left,right):
+    merge_list=[]
+    i,j=0,0
+    while(i<len(left)and j<len(right)):
+        if(left[i]<right[j]):
+            merge_list.append(left[i])
             i+=1
-            k+=1
-        while j<len(right):
-            myList[k]=right[j]
+        else:
+            merge_list.append(right[j])
             j+=1
-            k+=1
-x=int(input("enter the length of the list : "))
-myList = [int (input("enter the element in the list : ")) for i in range(x)]
-mergesort(myList)
-print(myList)
+    while(i<len(left)):
+        merge_list.append(left[i])
+        i+=1
+    while(j<len(right)):
+        merge_list.append(right[j])
+        j+=1
+    return merge_list
+
+li=[12,45,32,8,55,26,96,10]
+print(li)
+sorted_li=divide(li)
+print(sorted_li)
